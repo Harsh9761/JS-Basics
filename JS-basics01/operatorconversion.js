eccc53eb1118d260246312b919a47aea0bac0dbd
Harshdeep Singh <122586315+Harsh9761@users.noreply.github.com>
2024-06-26 08:08:25 +0000

    Operation done

--- JS-basics01/operatorconversion.js
+++ JS-basics01/operatorconversion.js
@@ -2,9 +2,9 @@ let score = 33
 
 let number = Number(score)
 
-console.log(typeof score);
-console.log(typeof (number));
-console.log(number);
+// console.log(typeof score);
+// console.log(typeof (number));
+// console.log(number);
 
 
 // 33=> 33
@@ -15,7 +15,7 @@ let isloggedIn = ""
 
 let booleanisloggedIn = Boolean(isloggedIn)
 
-console.log(booleanisloggedIn);
+// console.log(booleanisloggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,4 +25,45 @@ let someNumber = 33
 
 let StringNumber = String(someNumber)
 
-console.log(typeof StringNumber);
+// console.log(typeof StringNumber);
+
+// ***************************Operations*****************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " HArsh"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2+2 //Not recommended
+
+let counter = 100
+++counter
+counter++
+console.log(counter);
+
+
+
+
