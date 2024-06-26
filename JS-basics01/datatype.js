07c51cd69539e2a27120643627b1ac9d475f625f
Harshdeep Singh <122586315+Harsh9761@users.noreply.github.com>
2024-06-26 07:49:26 +0000

    Datatypes Discussion Done

---
+++ JS-basics01/datatype.js
@@ -0,0 +1,26 @@
+"use strict";//Treat all JS code as newer version
+
+// alert(3 + 3) //We are using Nodejs not browser
+
+console.log(3+3);
+
+console.log("Harsh"); //Code readability should be high
+
+
+let name = "Harsh"
+let age = 18
+let isloggedIn = true
+
+// Primitive Datatypes
+//number => 2 to power 53
+//bigint => used in trading,flipkart
+// String => ""
+// boolean => true/false 
+// null => Standalone value
+// Undefined => VAlue not Assigned
+// Symbol => Unique
+
+console.log(typeof name); //String
+console.log(typeof null); // Object
+console.log(typeof undefined); // Undefined
+
---
+++ JS-basics01/operatorconversion.js
@@ -0,0 +1,28 @@
+let score = 33
+
+let number = Number(score)
+
+console.log(typeof score);
+console.log(typeof (number));
+console.log(number);
+
+
+// 33=> 33
+// "33abc" => NaN
+//  true => 1; false => 0;
+
+let isloggedIn = ""
+
+let booleanisloggedIn = Boolean(isloggedIn)
+
+console.log(booleanisloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Harsh" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+
+console.log(typeof StringNumber);
