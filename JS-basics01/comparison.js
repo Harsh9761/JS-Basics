91e940362cb36e1cd3743733ea3bc05c1cdcc313
Harshdeep Singh <122586315+Harsh9761@users.noreply.github.com>
2024-06-26 08:18:51 +0000

    comparison done

---
+++ JS-basics01/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Equality operator and Comparison operstor works differently
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+// ===(Strict check)
+
+console.log("2" == 2);
